7c3343c1c88cfb709c3b6b40fd625f0a87acad9f
ZAIN HANIF <52876033+zainhanif73@users.noreply.github.com>
2024-01-28 16:22:00 +0100

    Update Header.jsx

--- src/MainPage/Header.jsx
+++ src/MainPage/Header.jsx
@@ -9,7 +9,7 @@ function Header({click, setClick}) {
         <div className='flex z-[100] relative justify-between mt-8'>
             <span className='flex ml-8'>
                 <img width={50} height={50} src={logo} alt="" />
-                <h1 className='text-[#ffffff] hover:text-[#ef2c2c] hover:cursor-pointer font-[600] text-3xl pt-2 ml-2' onClick={() => { router("/"); setClick(0); }}>Pinflex</h1>
+                <h1 className="header-logo" onClick={() => { router("/"); setClick(0); }}>Pinflex</h1>
             </span>
             <span className='mr-16 hidden sm:flex'>
                 <h1 className={'text-[#ffffff] ml-4 hover:text-[#ef2c2c] hover:cursor-pointer font-[550] text-[24px] pt-2 ml-2 ' + (click === 0 ? "border-b-2 border-[#ef2c2c]" : "")} style={{fontFamily:"'Montserrat',sans-serif"}} onClick={() => {setClick(0); router("/"); }}>Home </h1>
